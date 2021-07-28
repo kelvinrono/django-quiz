33e9ddc2fb5d41279e9ceb8a0c975aee15ecfa6d
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-07-28 16:08:13 +0300

    register the  models and create forms

---
+++ exam/forms.py
@@ -0,0 +1,14 @@
+from django.forms import ModelForm
+from .models import *
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+ 
+class Createuserform(UserCreationForm):
+    class Meta:
+        model=User
+        fields=['username','password'] 
+ 
+class AddQuestionform(ModelForm):
+    class Meta:
+        model=QuestionModel
+        fields="__all__"
