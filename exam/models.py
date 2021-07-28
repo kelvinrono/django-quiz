6aebc5108204ffa1ac3d7b1df0f5e372eeecd53c
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-07-28 15:57:09 +0300

    create the question and answer model

--- exam/models.py
+++ exam/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+ 
+# Create your models here.
+class QuestionModel(models.Model):
+    question = models.CharField(max_length=200,null=True)
+    answer1 = models.CharField(max_length=200,null=True)
+    answer2 = models.CharField(max_length=200,null=True)
+    answer2 = models.CharField(max_length=200,null=True)
+    answer2 = models.CharField(max_length=200,null=True)
+    answer2 = models.CharField(max_length=200,null=True)
+    
+    def __str__(self):
+        return self.question
