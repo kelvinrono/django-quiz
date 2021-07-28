10d4f7921168df79d391e4b37ca86d3e03509d1a
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-07-28 16:20:42 +0300

    create the register and login views

--- exam/views.py
+++ exam/views.py
@@ -52,3 +52,37 @@ def addQuestion(request):
         return render(request,'Quiz/addQuestion.html',context)
     else: 
         return redirect('home') 
+
+
+def registerPage(request):
+    if request.user.is_authenticated:
+        return redirect('home') 
+    else: 
+        form = createuserform()
+        if request.method=='POST':
+            form = createuserform(request.POST)
+            if form.is_valid() :
+                user=form.save()
+                return redirect('login')
+        context={
+            'form':form,
+        }
+        return render(request,'Quiz/register.html',context)
+ 
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+       if request.method=="POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('/')
+       context={}
+       return render(request,'Quiz/login.html',context)
+ 
+def logoutPage(request):
+    logout(request)
+    return redirect('/')
