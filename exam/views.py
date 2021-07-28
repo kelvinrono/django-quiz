f19cdad66d139d7971e11e587a10e4c79e709db1
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-07-28 16:16:22 +0300

    create the index view

--- exam/views.py
+++ exam/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect,render
+from django.contrib.auth import login,logout,authenticate
+from .forms import *
+from .models import *
+from django.http import HttpResponse
+ 
 # Create your views here.
+def home(request):
+    if request.method == 'POST':
+        print(request.POST)
+        questions=QuestionModel.objects.all()
+        score=0
+        wrong=0
+        correct=0
+        total=0
+        for question in questions:
+            total+=1
+            print(request.POST.get(question.question))
+            print(question.ans)
+            print()
+            if question.ans ==  request.POST.get(question.question):
+                score+=10
+                correct+=1
+            else:
+                wrong+=1
+        percent = score/(total*10) *100
+        context = {
+            'score':score,
+            'time': request.POST.get('timer'),
+            'correct':correct,
+            'wrong':wrong,
+            'percent':percent,
+            'total':total
+        }
+        return render(request,'Quiz/result.html',context)
+    else:
+        questions=QuestionModel.objects.all()
+        context = {
+            'questions':questions
+        }
+        return render(request,'Quiz/home.html',context)
+ 
